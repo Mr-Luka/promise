7a07b06378160993c5248ee1d862a794852a588c
Luka <luka.luka.luka93@gmail.com>
2024-04-26 17:15:52 -0700

    making pizza with a promise and .then()

--- main.js
+++ main.js
@@ -1,17 +1,29 @@
 function makePizza (toppings) {
      const pizzaPromise = new Promise (function (resolve, reject){
+        //Wait one second for pizza to cook
+        setTimeout(function(){
+            resolve(`Here is your pizza with toppings ${toppings.join(" ")}`)
+
+        }, 1000)
         // When you are ready, you can resolve this promise
-        resolve(`Here is your pizza with toppings ${toppings.join(" ")}`)
         //If something went wrong, we can reject this promise
      });
      return pizzaPromise;
 }
-
-const veggiePizza = makePizza(["broccoli", "artichoke"]);
-const veganPizza = makePizza(["onions", "mushrooms", "olives"]);
-
-
-veggiePizza.then(function(pizza){
-    console.log("Ahh I got it");
-    console.log(pizza)
-})
+console.log("First")
+makePizza(["broccoli", "artichoke"])
+    .then(function(pizza){
+        console.log(pizza);
+        return makePizza(["vegan cheese"]);
+    })
+    .then(function(pizza){
+        console.log(pizza);
+        return makePizza(["tomato"])
+    })
+    .then(function(pizza){
+        console.log(pizza)
+        return makePizza(["hot peppers", "onion", "apple"]);
+    }).then(function(pizza){
+        console.log(pizza)
+    });
+console.log("second")
