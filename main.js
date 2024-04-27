85d1ac6ba667e974d2ed617e566102cfd125c3b8
Luka <luka.luka.luka93@gmail.com>
2024-04-26 17:04:45 -0700

    console logging the pizzas and preparing for timer

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
-function makePizza () {
+function makePizza (toppings) {
      const pizzaPromise = new Promise (function (resolve, reject){
         // When you are ready, you can resolve this promise
-        resolve("pizzzaaaaa")
+        resolve(`Here is your pizza with toppings ${toppings.join(" ")}`)
         //If something went wrong, we can reject this promise
      });
      return pizzaPromise;
 }
 
-const pizza = makePizza();
-console.log(pizza)
+const veggiePizza = makePizza(["broccoli", "artichoke"]);
+const veganPizza = makePizza(["onions", "mushrooms", "olives"]);
+
+
+veggiePizza.then(function(pizza){
+    console.log("Ahh I got it");
+    console.log(pizza)
+})
