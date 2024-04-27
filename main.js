62dc4f440d37ecaa3cc86d254a9c02e1a1b94689
Luka <luka.luka.luka93@gmail.com>
2024-04-26 19:36:25 -0700

    made all burgers and the fastest one using Promise.all and Promise.race

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
-function makePizza (toppings = []) {
-     const pizzaPromise = new Promise (function (resolve, reject){
-        const amountOfTimeToBake = 500 + (toppings.length * 200)
-        //Wait one second for pizza to cook
-        setTimeout(function(){
-            resolve(`Here is your pizza with toppings ${toppings.join(" ")}`)
+// function makePizza (toppings = []) {
+//      const pizzaPromise = new Promise (function (resolve, reject){
+//         const amountOfTimeToBake = 500 + (toppings.length * 200)
+//         //Wait one second for pizza to cook
+//         setTimeout(function(){
+//             resolve(`Here is your pizza with toppings ${toppings.join(" ")}`)
 
-        }, amountOfTimeToBake)
-        // When you are ready, you can resolve this promise
-        //If something went wrong, we can reject this promise
-     });
-     return pizzaPromise;
-}
+//         }, amountOfTimeToBake)
+//         // When you are ready, you can resolve this promise
+//         //If something went wrong, we can reject this promise
+//      });
+//      return pizzaPromise;
+// }
 // console.log("First")
 // makePizza(["broccoli", "artichoke"])
 //     .then(function(pizza){
@@ -36,30 +36,44 @@ function makePizza (toppings = []) {
 // console.log("second")
 
 // Run them concurrently
-const pizzaPromise1 = makePizza(["vegan cheese", "tomato", "mushrooms"]);
-const pizzaPromise2 = makePizza(["one", "two", "three", "four", "five", "six", "one", "two", "three", "four", "five", "six"]);
-const pizzaPromise3 = makePizza(["vegan ham", "vegan cheese"]);
+// const pizzaPromise1 = makePizza(["vegan cheese", "tomato", "mushrooms"]);
+// const pizzaPromise2 = makePizza(["one", "two", "three", "four", "five", "six", "one", "two", "three", "four", "five", "six"]);
+// const pizzaPromise3 = makePizza(["vegan ham", "vegan cheese"]);
 
-const dinnerPromise = Promise.all([pizzaPromise1, pizzaPromise2, pizzaPromise3]);
+// const dinnerPromise = Promise.all([pizzaPromise1, pizzaPromise2, pizzaPromise3]);
 
-dinnerPromise.then(function([hottie, garbagePail, tomatoAndBasil]){
-    console.log(hottie, garbagePail, tomatoAndBasil)
-})
+// dinnerPromise.then(function([hottie, garbagePail, tomatoAndBasil]){
+//     console.log(hottie, garbagePail, tomatoAndBasil)
+// })
 
-const firstPizzaPromise = Promise.race([pizzaPromise1, pizzaPromise2, pizzaPromise3]);
+// const firstPizzaPromise = Promise.race([pizzaPromise1, pizzaPromise2, pizzaPromise3]);
 
-firstPizzaPromise.then(pizza=>{
-    console.log("you must be hungry");
-    console.log(pizza)
-})
+// firstPizzaPromise.then(pizza=>{
+//     console.log("you must be hungry");
+//     console.log(pizza)
+// })
 
 
 function makeBurger (inside=[]) {
     return new Promise (function (resolve, reject){
-        const timeToMakeBurger = 500 (inside.length *200);
+        const timeToMakeBurger = 500 + (inside.length *200);
         setTimeout(function(){
             resolve(`Here is your burger with ${inside.join(" ")}`)
         }, timeToMakeBurger);
     });
 }
+
+const burgerPromise = makeBurger(["lettuce", "vegan patty", "tomato", "pickles", "vegan mayo", "mushrooms", "vegan patty", "tomato", "pickles", "vegan mayo", "vegan patty", "tomato", "pickles", "vegan mayo"]);
+const burgerPromise2 = makeBurger(["tomato", "vegan patty"]);
+const burgerPrpmise3 = makeBurger(["vegan patty"]);
+
+const burgersPromise = Promise.all([burgerPromise, burgerPromise2, burgerPrpmise3]);
+burgersPromise.then(function([bla, blabla, blablabla]){
+    console.log(bla, blabla, blablabla);
+})
+
+const fastestBurger = Promise.race([burgerPromise, burgerPromise2, burgerPrpmise3]);
+fastestBurger.then(burg=>console.log(burg))
+
+
 
